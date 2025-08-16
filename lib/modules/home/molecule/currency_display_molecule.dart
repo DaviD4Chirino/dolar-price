3f45224e1f706da615293f1062381856df2fb9e1
David Space <genarodavid@gmail.com>
2025-08-16 16:51:44 -0400

    added currency symbol

--- lib/modules/home/molecule/currency_display_molecule.dart
+++ lib/modules/home/molecule/currency_display_molecule.dart
@@ -1,3 +1,6 @@
+import 'package:awesome_dolar_price/extensions/double_extensions/sized_box_extension.dart';
+import 'package:awesome_dolar_price/tokens/app/app_spacing.dart';
+import 'package:currency_code_to_currency_symbol/currency_code_to_currency_symbol.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyDisplayMolecule extends StatelessWidget {
@@ -17,7 +20,17 @@ class CurrencyDisplayMolecule extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        Text(currency, style: theme.textTheme.bodyLarge),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CurrencyToSymbolWidget(
+              currencyCode: currency,
+              textStyle: theme.textTheme.bodyLarge,
+            ),
+            AppSpacing.sm.sizedBoxW,
+            Text(currency, style: theme.textTheme.bodyLarge)
+          ],
+        ),
         Text(value, style: theme.textTheme.bodyLarge),
       ],
     );
