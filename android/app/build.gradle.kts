548b03d8a7841a0b8eba6d789b4efbb93667b9a2
David Space <genarodavid@gmail.com>
2025-09-03 19:16:24 -0400

    updated the icons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         applicationId = "com.davidspace.awesome_dolar_price"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 27.0.12077973
+        minSdk = 28
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
