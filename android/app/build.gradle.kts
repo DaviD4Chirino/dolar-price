6bfda776bf41641928945c2430f7b7ee9937a063
David Space <genarodavid@gmail.com>
2025-10-15 22:47:57 -0400

    fixed an error where the app was not being signed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -76,6 +76,7 @@ android {
         getByName("release") {
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+            signingConfig = signingConfigs.getByName("release")
         }
 
         getByName("debug") {
