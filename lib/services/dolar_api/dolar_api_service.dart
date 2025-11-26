607d2d1eac77403e3ab6fe92968e054639112f58
David Space <genarodavid@gmail.com>
2025-11-26 15:28:55 -0400

    updated the supported currencies for dolar api

--- lib/services/dolar_api/dolar_api_service.dart
+++ lib/services/dolar_api/dolar_api_service.dart
@@ -1,3 +1,4 @@
+import 'package:currency_code_to_currency_symbol/currency_code_to_currency_symbol.dart';
 import 'package:doya/services/exchange_rate/models/supported_currency.dart';
 import 'package:doya/tokens/constants/rate_source.dart';
 
@@ -7,7 +8,19 @@ abstract class DolarApiService {
     return [
       SupportedCurrency(
         code: "USD",
-        name: "Dolar (Paralelo)",
+        name: "Dolar",
+        symbol: "\$",
+        source: RateSource.dolarApi,
+      ),
+      SupportedCurrency(
+        code: "BTC",
+        name: "Dolar",
+        symbol: getCurrencySymbol("BTC"),
+        source: RateSource.dolarApi,
+      ),
+      SupportedCurrency(
+        code: "USD",
+        name: "Dólar Paralelo",
         symbol: "\$",
         source: RateSource.dolarApi,
       ),
